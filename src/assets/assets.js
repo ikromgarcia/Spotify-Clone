ed1300844fbb8a69f27497aed6c8d13d27d67a59
Ikromgarcia <mrdomer7889@gmail.com>
2024-12-11 19:51:58 +0500

    Spotify-Clone

---
+++ src/assets/assets.js
@@ -0,0 +1,715 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './macan1.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img21 from './6.3.jpg'
+import img22 from './asphalt8.jfif'
+import img15 from './navai-albom.jpg'
+import img16 from './img16.jpg'
+import img17 from './zari_do_zari.jpg'
+import img18 from './navai.jpeg' 
+import img19 from './merin.jpg'
+import img20 from './mama_eh.jfif'
+import img23 from './malo.jpeg'
+import img24 from './ya-xo4y.jpeg'
+import img25 from './dengi.jpeg'
+import img26 from './gollows.jpeg'
+import img27 from './ferrari.jpeg'
+import img29 from './nimbooda.jpg'
+import img28 from './kuch.jpg'
+import img30 from './uzb1.png'
+import img31 from './uzb2.jpeg'
+import img32 from './uzb3.jpeg'
+import img33 from './uzb4.jpg'
+import img34 from './uzb5.jpeg'
+import img35 from './uzb6.jpg'
+import img36 from './uzb7.jpg'
+import img37 from './uzb8.jpg'
+import img38 from './uzb9.png'
+import img39 from './uzb10.jpeg'
+import img40 from './gl2.jpeg'
+import img41 from './gl3.png'
+import img42 from './gl4.jpeg'
+import img43 from './gl5.png'
+import img44 from './gl6.jpeg'
+import img45 from './gl7.jpeg'
+import img46 from './gl8.jpeg'
+import img47 from './gl9.jpeg'
+import img48 from './gl10.png'
+import img49 from './macan1.jpg'
+import img50 from './navai1.jpeg'
+import img51 from './navai2.jpeg'
+import img52 from './navai3.png'
+import img53 from './navai4.jpeg'
+import img54 from './navai5.jpeg'
+import img55 from './navai6.jpeg'
+import img56 from './navai7.jpeg'
+import img57 from './navai8.jpeg'
+import img58 from './navai9.jpeg'
+import img59 from './navai10.jpeg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from './6xcho3.mp3'
+import song8 from './asphalt_8.mp3'
+import song9 from './navai_mona.mp3'
+import song10 from './mamaeh.mp3'
+import song11 from './merin_xcho.mp3'
+import song12 from './zari.mp3'
+import song13 from './Ulukmanapo, Bakr - Мало и мало.mp3'
+import song14 from './MACAN, SCIRENA - Я хочу с тобой.mp3'
+import song15 from './moskva.mp3'
+import song16 from './white.mp3'
+import song17 from './ferari.mp3'
+import song18 from './kuhc.mp3'
+import song19 from './nimboda.mp3'
+import song20 from './uzb-1.mp3'
+import song21 from './uzb-2.mp3'
+import song22 from './uzb-3.mp3'
+import song23 from './uzb-4.mp3'
+import song24 from './uzb-5.mp3'
+import song25 from './uzb-6.mp3'
+import song26 from './uzb-7.mp3'
+import song27 from './uzb-8.mp3'
+import song28 from './uzb-9.mp3'
+import song29 from './uzb-10.mp3'
+import song30 from './gl-2.mp3'
+import song31 from './gl-3.mp3'
+import song32 from './gl-4.mp3'
+import song33 from './gl-5.mp3'
+import song34 from './gl-6.mp3'
+import song35 from './gl-7.mp3'
+import song36 from './gl-8.mp3'
+import song37 from './gl-9.mp3'
+import song38 from './gl-10.mp3'
+import song39 from './macan-1.mp3'
+import song40 from './macan-2.mp3'
+import song41 from './macan-3.mp3'
+import song42 from './macan-4.mp3'
+import song43 from './macan-5.mp3'
+import song44 from './macan-6.mp3'
+import song45 from './macan-7.mp3'
+import song46 from './macan-8.mp3'
+import song47 from './macan-9.mp3'
+import song48 from './macan-10.mp3'
+import song50 from './navai-1.mp3'
+import song51 from './navai-2.mp3'
+import song52 from './navai-3.mp3'
+import song53 from './navai-4.mp3'
+import song54 from './navai-5.mp3'
+import song55 from './navai-6.mp3'
+import song56 from './navai-7.mp3'
+import song57 from './navai-8.mp3'
+import song58 from './navai-9.mp3'
+import song59 from './navai-10.mp3'
+
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Russia",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365",
+        file:[song7,song12, song8,song9,song11,song13,song14,song15,song16,song17]
+        
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d",
+        file:[song1,song2,song3,song4,song5,song6,song18,song19]
+    },
+    {   
+        id:2,
+        name: "Trending Uzbekistan",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a",
+        file:[song20,song21,song22,song23,song24,song25,song26,song27,song28,song29]
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a",
+        file:[song10,song30,song31,song32,song33,song34,song35,song36,song37,song38]
+    },
+    {   
+        id:4,
+        name: "MACAN",
+        image: img11,
+        desc:"Popular songs from singer - MACAN",
+        bgColor:"#234e52",
+        file:[song39,song40,song41,song42,song43,song44,song45,song46,song47,song48]
+    },
+    {   
+        id:5,
+        name: "Navai",
+        image: img15,
+        desc:"Popular songs from singer - Navai",
+        bgColor:"#744210",
+        file:[song50,song51,song52,song53,song54,song55,song56,song57,song58,song59]
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Kutti Story",
+        image: img1,
+        file:song1,
+        desc:"Aniruth Ravichander,Thalapathy Vijay",
+        duration:"5:22"
+    },
+    {
+        id:1,
+        name: "VIP Title Song",
+        image: img2,
+        file:song2,
+        desc:"Anirudh Ravichander",
+        duration:"2:43"
+    },
+    {
+        id:2,
+        name: "Dharala Prabhu Title Track",
+        image: img3,
+        file:song3,
+        desc:"Anirudh Ravichander",
+        duration:"3:42"
+    },
+    {
+        id:3,
+        name: "Hukum-Thalaivar Alappara",
+        image: img4,
+        file:song4,
+        desc:"Anirudh Ravichander,Super Subu",
+        duration:"3:26"
+    },
+    {
+        id:4,
+        name: "Illuminati",
+        image: img5,
+        file:song5,
+        desc:"Sushin Shyam, Dabzee, Vinayak Sasikumar",
+        duration:"2:28"
+    },
+    {
+        id:5,
+        name: "Idhazin Oram-The Innocence of Love",
+        image: img6,
+        file:song6,
+        desc:"Anirudh Ravichander,Ajesh",
+        duration:"3:24"
+    },
+    {
+        id:6,
+        name: "Vaathi Coming",
+        image: img7,
+        file:song1,
+        desc:"Aniruth Ravichander,Thalapathy Vijay",
+        duration:"3:48"
+    },
+    {
+        id:7,
+        name: "What a Karavaad",
+        image: img2,
+        file:song2,
+        desc:"Anirudh Ravichander,Dhanush",
+        duration:"4:17"
+    },
+    {
+        id:8,
+        name: "Зари",
+        image: img17,
+        file:song12,
+        desc:"Andro, ELMAN, TONI, MONA",
+        duration:"2:52"
+    },
+    {
+        id:9,
+        name: "Xcho-6.3",
+        image: img21,
+        file:song7,
+        desc:"Xcho",
+        duration:"2:09"
+    },
+    {
+        id:10,
+        name: "Asphalt 8",
+        image: img22,
+        file:song8,
+        desc:"Macan",
+        duration:"2:16"
+    },
+    {
+        id:11,
+        name: "Безусловная любовь",
+        image: img18,
+        file:song9,
+        desc:"Navai, Mona",
+        duration:"2:40"
+    },
+    {
+        id:12,
+        name: "Mr Lambo & Xcho & Пабло - Мерин",
+        image: img19,
+        file:song11,
+        desc:"Mr Lambo & Xcho & Пабло",
+        duration:"2:10"
+    },
+    {
+        id:13,
+        name: "Мало и Мало",
+        image: img23,
+        file:song13,
+        desc:"Ulukmanapo, Bakr",
+        duration:"3:41"
+    },
+    {
+        id:14,
+        name: "Я хочу с тобой",
+        image: img24,
+        file:song14,
+        desc:"MACAN, SCIRENA",
+        duration:"3:18"
+    },
+    {
+        id:15,
+        name: "Деньги И Москва",
+        image: img25,
+        file:song15,
+        desc:"SCIRENA",
+        duration:"2:26"
+    },
+    {
+        id:16,
+        name: "Сердце",
+        image: img26,
+        file:song16,
+        desc:"WHITE GALLOWS",
+        duration:"2:12"
+    },
+    {
+        id:17,
+        name: "Ferrari",
+        image: img27,
+        file:song17,
+        desc:"WHITE GALLOWS, SCIRENA",
+        duration:"2:52"
+    },
+    {
+        id:18,
+        name: "Kuch Kuch Hota Hai",
+        image: img28,
+        file:song18,
+        desc:"Sad",
+        duration:"4:57"
+    },
+    {
+        id:19,
+        name: "Nimbooda",
+        image: img29,
+        file:song19,
+        desc:"Karsan Sagathia & Kavita Krishnamurthy",
+        duration:"6:24"
+    },
+    {
+        id:20,
+        name: "Qizg'onaman",
+        image: img30,
+        file:song20,
+        desc:"Mahliyo",
+        duration:"2:41"
+    },
+    {
+        id:21,
+        name: "Telba",
+        image: img31,
+        file:song21,
+        desc:"Mahliyo",
+        duration:"3:51"
+    },
+    {
+        id:22,
+        name: "Sevmasang bor",
+        image: img32,
+        file:song22,
+        desc:"Barhayot Umarov",
+        duration:"3:01"
+    },
+    {
+        id:23,
+        name: "Boshqacha",
+        image: img33,
+        file:song23,
+        desc:"Mahliyo",
+        duration:"2:53"
+    },
+    {
+        id:24,
+        name: "Janona",
+        image: img34,
+        file:song24,
+        desc:"G'aybulla Tursunov",
+        duration:"4:08"
+    },
+    {
+        id:25,
+        name: "Sharara",
+        image: img35,
+        file:song25,
+        desc:"Mahliyo",
+        duration:"3:03"
+    },
+    {
+        id:26,
+        name: "Yor",
+        image: img36,
+        file:song26,
+        desc:"G'aybulla Tursunov",
+        duration:"3:44"
+    },
+    {
+        id:27,
+        name: "Yuragimga",
+        image: img37,
+        file:song27,
+        desc:"Mahliyo",
+        duration:"2:32"
+    },
+    {
+        id:28,
+        name: "Zaharli iloncham",
+        image: img38,
+        file:song28,
+        desc:"Donyor Ismoilov",
+        duration:"2:23"
+    },
+    {
+        id:29,
+        name: "Yomg'ir",
+        image: img39,
+        file:song29,
+        desc:"Mahliyo",
+        duration:"2:28"
+    },
+    {
+        id:30,
+        name: "Mama Eh",
+        image: img20,
+        file:song10,
+        desc:"Elyanna",
+        duration:"2:32"
+    },
+    {
+        id:31,
+        name: "Gata Only",
+        image: img40,
+        file:song30,
+        desc:"FloyyMenor & Cris Mj",
+        duration:"3:42"
+    },
+    {
+        id:32,
+        name: "APT.",
+        image: img41,
+        file:song31,
+        desc:"ROSE,Bruno Mars",
+        duration:"2:49"
+    },
+    {
+        id:33,
+        name: "That's So True",
+        image: img42,
+        file:song32,
+        desc:"Gracie Abrams",
+        duration:"2:46"
+    },
+    {
+        id:34,
+        name: "Birds Of a Feather",
+        image: img43,
+        file:song33,
+        desc:"Billie Eilish",
+        duration:"3:30"
+    },
+    {
+        id:35,
+        name: "Taste",
+        image: img44,
+        file:song34,
+        desc:"Sabrina Carpenter",
+        duration:"2:37"
+    },
+    {
+        id:36,
+        name: "Die With a Smile",
+        image: img45,
+        file:song35,
+        desc:"Lady Gaga",
+        duration:"4:11"
+    },
+    {
+        id:37,
+        name: "Good luck,babe!",
+        image: img46,
+        file:song36,
+        desc:"Chappell Roan",
+        duration:"3:38"
+    },
+    {
+        id:38,
+        name: "Toxic Till The End",
+        image: img47,
+        file:song37,
+        desc:"ROSE",
+        duration:"2:36"
+    },
+    {
+        id:39,
+        name: "WILDFLOWER",
+        image: img48,
+        file:song38,
+        desc:"Billie Eilish",
+        duration:"4:21"
+    },
+
+    {
+        id:40,
+        name: "I AM",
+        image: img49,
+        file:song39,
+        desc:"MACAN",
+        duration:"3:05"
+    },
+    {
+        id:41,
+        name: "Интро",
+        image: img49,
+        file:song40,
+        desc:"MACAN",
+        duration:"2:03"
+    },
+    {
+        id:42,
+        name: "TSM",
+        image: img49,
+        file:song41,
+        desc:"MACAN",
+        duration:":"
+    },
+    {
+        id:43,
+        name: "Занова",
+        image: img49,
+        file:song42,
+        desc:"MACAN",
+        duration:"2:00"
+    },
+    {
+        id:44,
+        name: "Привыкаю",
+        image: img49,
+        file:song43,
+        desc:"MACAN,A.V.G",
+        duration:"2:54"
+    },
+    {
+        id:45,
+        name: "Москва бежит",
+        image: img49,
+        file:song44,
+        desc:"MACAN",
+        duration:"2:25"
+    },
+    {
+        id:46,
+        name: "Salut",
+        image: img49,
+        file:song45,
+        desc:"MACAN",
+        duration:"2:30"
+    },
+    {
+        id:47,
+        name: "Сгорел",
+        image: img49,
+        file:song46,
+        desc:"MACAN",
+        duration:"2:09"
+    },
+    {
+        id:48,
+        name: "А у меня",
+        image: img49,
+        file:song47,
+        desc:"MACAN",
+        duration:"2:09"
+    },
+    {
+        id:49,
+        name: "Всё правильно",
+        image: img49,
+        file:song48,
+        desc:"MACAN,Jakone",
+        duration:"1:47"
+    },
+
+    {
+        id:50,
+        name: "Сейфспейс",
+        image: img50,
+        file:song50,
+        desc:"Джарахов feat.Navai",
+        duration:"2:44"
+    },
+
+    {
+        id:51,
+        name: "Безусловная любовь",
+        image: img51,
+        file:song51,
+        desc:"MONA,Navai",
+        duration:"2:46"
+    },
+
+    {
+        id:52,
+        name: "Любовь, похожая на сон",
+        image: img52,
+        file:song52,
+        desc:"Navai",
+        duration:"3:42"
+    },
+
+    {
+        id:53,
+        name: "История Недоступна",
+        image: img53,
+        file:song53,
+        desc:"Миша Марвин & Navai",
+        duration:"2:52"
+    },
+
+    {
+        id:54,
+        name: "Всем заметно",
+        image: img54,
+        file:song54,
+        desc:" HammAli & Navai",
+        duration:"3:33"
+    },
+
+    {
+        id:55,
+        name: "Аэропорт",
+        image: img55,
+        file:song55,
+        desc:"OG MINAY, LIL KAVKAZ, Navai",
+        duration:"3:25"
+    },
+
+    {
+        id:56,
+        name: "Лепесток",
+        image: img56,
+        file:song56,
+        desc:"HammAli & Navai, Loc-Dog",
+        duration:"3:17"
+    },
+
+    {
+        id:57,
+        name: "Синяя Вечность",
+        image: img57,
+        file:song57,
+        desc:"Navai",
+        duration:"3:10"
+    },
+
+    {
+        id:58,
+        name: "Будь Сильнее",
+        image: img58,
+        file:song58,
+        desc:"Navai",
+        duration:"1:46"
+    },
+
+    {
+        id:59,
+        name: "Холодно",
+        image: img59,
+        file:song59,
+        desc:"Navai.feat Любовь Успенская",
+        duration:"4:21"
+    },
+]
